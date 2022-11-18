dea8cd30cb9e181b71f64f6987f7a818643c1960
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-11-18 23:01:03 +0100

    websocket server setup

---
+++ server/index.ts
@@ -0,0 +1,29 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
+import cors from "cors";
+
+dotenv.config();
+
+const app: Express = express();
+app.use(cors());
+
+const port = process.env.PORT;
+const io = new Server<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+>();
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+});
+
+app.listen(port, () => {
+    console.log("listening on *:3000");
+});
